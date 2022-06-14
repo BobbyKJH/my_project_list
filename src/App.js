3d059b9991e429e0ea4089476808c95c72df1241
BobbyKJH <rlatnguscjsw@gmail.com>
2022-06-15 01:02:19 +0900

    feat: header.jsx router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route to="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 
+const Box = styled.div`
+  display: block;
+  height: 120px;
+`;
+
 const Container = styled.div`
   display: block;
-  width: 100vw;
-  height: 300px;
-  border: 1px solid black;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  height: 100px;
+  width: 90vw;
+  /* border: 1px solid black; */
+  border-radius: 20px;
+  background-color: #000;
+  transition: 1s;
+  &:hover {
+    background-color: #fff;
+  }
 `;
 
 function Header() {
-  return <Container>Header</Container>;
+  return (
+    <Box>
+      <Container>1111</Container>
+    </Box>
+  );
 }
 
 export default Header;
